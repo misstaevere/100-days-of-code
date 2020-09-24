3c1d928689a0956205757600eb6b8e39dfae4d33
misstaevere <kartaevere@gmail.com>
2020-09-24 17:55:15 +0100

    Add files via upload

---
+++ Section 13/advanced_exercise_3/exercise3.js
@@ -0,0 +1,64 @@
+// change everything below to the newer Javascript!
+
+// let + const
+let a = 'test';
+const b = true;
+const c = 789;
+a = 'test2';
+
+
+// Destructuring
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+// let eyeColor = person.eyeColor;
+
+const { firstName, lastName, age } = person;
+let { eyeColor } = person;
+
+// Object properties
+let a = 'test';
+let b = true;
+let c = 789;
+
+let okObj = { a, b, c };
+
+// Template strings
+// var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+let message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`
+
+// default arguments
+// default age to 10;
+// function isValidAge(age) {
+//     return age
+// }
+
+const isValidAge = (age=10) => age;
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+let sym = Symbol("This is my first Symbol");
+
+// Arrow functions
+// function whereAmI(username, location) {
+//     if (username && location) {
+//         return "I am not lost";
+//     } else {
+//         return "I am totally lost!";
+//     }
+// }
+
+const whereAmI = (username, location) => {
+  if (username && location) {
+    return "I am not lost";
+  } else {
+    return "I am totally lost";
+  }
+}
